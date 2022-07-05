b7cbaf1de929e3fd62deb7bd912e4a955e04db4e
Ignacio Yz <ignacioyz@gmail.com>
2022-07-05 13:38:55 -0400

    commit tree da5810be09a5dd0b515c1dc5ba8d4558db7a1452

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY, 
+name TEXT, 
+age INTEGER,
+sex TEXT, 
+color TEXT, 
+temperament TEXT, 
+alive, INTEGER
+)
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr. Chocolate", 20, "M", "dark brown", "calm")
+("Rowdy", 10, "M", "black", "intense", 1)
+("Tabitha", 6, "F", "dark brown", "Nice", 1)
+("Sergeant Brown", 19, "M", "Green", "Slimy", 0)
+("Melissa", 13, "F", "dark brown", "goofy", 1)
+("Grinch", 2, "M", "Black", "Grinchy", 1)
+("Wendy", 6, "F", "Blue", "naive", 1)
+("null", 20, "M", "black", "aggressive", 0)
